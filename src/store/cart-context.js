f00ed680bbeb93a00216fe30095309ee7d1b88f7
mounika1777 <madishettimounika1777@gmail.com>
2021-05-06 10:24:48 +0530

    UI for food order application

---
+++ src/store/cart-context.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const CartContext = React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (items) => {},
+    removeItem: (id) => {}
+});
+export default CartContext;
